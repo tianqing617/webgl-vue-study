5af2e9fe793980c44abc1f567a221f5c733c86c3
Tianqing Liu <lhard@qq.com>
2023-12-06 09:48:10 +0800

    docs: path说明

--- src/views/demo/RoughView.vue
+++ src/views/demo/RoughView.vue
@@ -20,6 +20,7 @@ function drawRectangle(el:HTMLCanvasElement) {
 function drawHill(el:HTMLCanvasElement) {
   const rc = rough.canvas(el)
   const hillOpts = { roughness: 2.8, strokeWidth: 2, fill: 'blue' }
+  // M 表示 Move to，L 表示 Line to
   rc.path('M76 256L176 156L276 256', hillOpts)
 }
 
