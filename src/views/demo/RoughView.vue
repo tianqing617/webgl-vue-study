c2b790951e21d53b357fd79084b6d7ea9ce67726
Liu Tianqing <lhard@qq.com>
2023-12-06 23:15:48 +0800

    feat: 变换坐标系绘制

--- src/views/demo/RoughView.vue
+++ src/views/demo/RoughView.vue
@@ -17,8 +17,10 @@ function drawRectangle(el:HTMLCanvasElement) {
   rc.rectangle(10, 10, 80, 20); // x, y, width, height
 }
 
+// 使用 坐标系与坐标映射 的方式
 function drawHill(el:HTMLCanvasElement) {
   const rc = rough.canvas(el)
+  console.log('drawHill', rc)
   const hillOpts = { roughness: 2.8, strokeWidth: 2, fill: 'blue' }
   // M 表示 Move to，L 表示 Line to
   rc.path('M76 256L176 156L276 256', hillOpts)
@@ -32,12 +34,35 @@ function drawHill(el:HTMLCanvasElement) {
   })
 }
 
+// 使用 坐标系转换 的方式
+function drawHill2(el:HTMLCanvasElement) {
+  const rc = rough.canvas(el)
+  const hillOpts = { roughness: 2.8, strokeWidth: 2, fill: 'blue' }
+  console.log('rc', rc)
+  // 转换坐标系
+  // TODO: 搜索替代方案
+  rc.ctx.translate(256, 256)
+  rc.ctx.scale(1, -1)
+  
+  // M 表示 Move to，L 表示 Line to
+  rc.path('M-180 0L-80 100L20 0', hillOpts)
+  rc.path('M-20 0L80 100L180 0', hillOpts)
+
+  rc.circle(0, 150, 105, {
+    stroke: 'red',
+    strokeWidth: 4,
+    fill: 'rgba(255, 255, 0, 0.4)',
+    fillStyle: 'solid',
+  })
+}
+
 const canvasRef = ref<HTMLCanvasElement | null>(null)
 onMounted(() => {
   console.log('ref', canvasRef)
   if (canvasRef.value) {
     drawRectangle(canvasRef.value)
-    drawHill(canvasRef.value)
+    // drawHill(canvasRef.value)
+    drawHill2(canvasRef.value)
   }
 })
 </script>
