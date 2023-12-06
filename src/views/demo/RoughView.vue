a0ef86cecd80a34ec05df77e8746b336b6e2b52a
Liu Tianqing <lhard@qq.com>
2023-12-06 22:39:03 +0800

    feat: 绘出太阳

--- src/views/demo/RoughView.vue
+++ src/views/demo/RoughView.vue
@@ -22,6 +22,14 @@ function drawHill(el:HTMLCanvasElement) {
   const hillOpts = { roughness: 2.8, strokeWidth: 2, fill: 'blue' }
   // M 表示 Move to，L 表示 Line to
   rc.path('M76 256L176 156L276 256', hillOpts)
+  rc.path('M236 256L336 156L436 256', hillOpts)
+
+  rc.circle(256, 106, 105, {
+    stroke: 'red',
+    strokeWidth: 4,
+    fill: 'rgba(255, 255, 0, 0.4)',
+    fillStyle: 'solid',
+  })
 }
 
 const canvasRef = ref<HTMLCanvasElement | null>(null)
