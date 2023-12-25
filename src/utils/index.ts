22169b809012f8433cb28e10625fcf7d7174adf7
Tianqing Liu <lhard@qq.com>
2023-12-25 18:40:40 +0800

    feat: 画抛物线

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 const TAU_SEGMENTS = 60
 const TAU = Math.PI * 2
+const LINE_SEGMENTS = 60
 
 /**
  * 画圆
@@ -44,6 +45,28 @@ export function ellipse(x0: number, y0: number, radiusX: number, radiusY: number
   for(let i = 0; i <= segments; i++) {
     const x = x0 + radiusX * Math.cos(startAng + ang * i / segments)
     const y = y0 + radiusY * Math.sin(startAng + ang * i / segments)
+
+    ret.push([x, y])
+  }
+
+  return ret
+}
+
+/**
+ * 画抛物线
+ * @param x0 抛物线在x轴上的顶点
+ * @param y0 抛物线在y轴上的顶点
+ * @param p 焦点到准线的距离
+ * @param min 最小弧长
+ * @param max 最大弧长
+ */
+export function parabola(x0: number, y0: number, p: number, min: number, max: number) {
+  const ret = [] as [number, number][]
+  for(let i = 0; i <= LINE_SEGMENTS; i++) {
+    const s = i / 60
+    const t = min * (1 - s) + max * s
+    const x = x0 + 2 * p * t **2
+    const y = y0 + 2 * p * t
 
     ret.push([x, y])
   }
--- src/views/demo/ArcView.vue
+++ src/views/demo/ArcView.vue
@@ -6,40 +6,47 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import { arc, ellipse } from '@/utils/index'
+import { arc, ellipse, parabola } from '@/utils/index'
 
 const canvasRef = ref<HTMLCanvasElement | null>(null)
 
 function drawLine(ret: [number, number][], ctx: CanvasRenderingContext2D) {
+  ctx.beginPath()
   ctx.moveTo(...ret[0])
   ret.forEach(item => {
     ctx.lineTo(...item)
   })
+
+  ctx.stroke()
 }
 
 function drawArc(ctx: CanvasRenderingContext2D) {
   const ret = arc(0, 0, 100, 0, Math.PI)
   console.log('ret', ret)
   // ctx.save()
-  ctx.beginPath()
+  // 开始一个新的路径
   ctx.strokeStyle = 'black';
   drawLine(ret, ctx)
-  ctx.stroke()
-  ctx.closePath()
+
+  // 使用closePath闭合路径
+  // ctx.closePath()
   // ctx.restore()
 }
 
 function drawEllipse(ctx: CanvasRenderingContext2D) {
   const ret = ellipse(0, 0, 200, 100)
   console.log('ret', ret)
-  // ctx.save()
-  ctx.beginPath()
   ctx.strokeStyle = 'blue';
   drawLine(ret, ctx)
-  ctx.stroke()
-  ctx.closePath()
-  // ctx.restore()
 }
+
+function drawParabola(ctx: CanvasRenderingContext2D) {
+  const ret = parabola(0, 0, 4, -Math.PI, Math.PI)
+  console.log('ret', ret)
+  ctx.strokeStyle = 'red'
+  drawLine(ret, ctx)
+}
+
 onMounted(() => {
   const ctx = canvasRef.value?.getContext('2d')
   // console.log('ctx', ctx)
@@ -50,6 +57,7 @@ onMounted(() => {
 
     drawArc(ctx)
     drawEllipse(ctx)
+    drawParabola(ctx)
   }
 })
 </script>
