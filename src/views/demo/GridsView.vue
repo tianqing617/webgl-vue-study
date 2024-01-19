3624c03908a7edda2225c29449e40086ccf6a52f
Tianqing Liu <lhard@qq.com>
2024-01-19 10:30:59 +0800

    feat: optimization

--- src/views/demo/GridsView.vue
+++ src/views/demo/GridsView.vue
@@ -23,6 +23,13 @@ const vertex = `
 `;
 
 // fract: 获取一个数的小数部分。
+// TODO: 片源着色器main后的前两行，不太理解
+/**
+ * step(0.5, 0.7); 返回1；交换两数位置，返回0
+ * 
+ * d1 * d2 = 1，则vec3(1.0) 白色（st.x > 0.9 & st.y > 0.1）
+ * d1 * d2 = 0，则vec3(0.8) 灰色(st.x < 0.9 & st.y < 0.1)
+*/
 const fragment = `
   #ifdef GL_ES
   precision mediump float;
@@ -35,7 +42,7 @@ const fragment = `
     vec2 st = fract(vUv * rows);
     float d1 = step(st.x, 0.9);
     float d2 = step(0.1, st.y);
-    gl_FragColor.rgb = mix(vec3(0.8), vec3(1.0), d1 * d2);
+    gl_FragColor.rgb = mix(vec3(0.8), vec3(1.0), d2 * d1);
     gl_FragColor.a = 1.0;
   }
 `;
@@ -50,8 +57,8 @@ function drawGrids(canvas: HTMLCanvasElement) {
   renderer.useProgram(program)
 
   // 1 4 16 32 64
-  // renderer.uniforms.rows = 1
-  renderer.uniforms.rows = 4
+  renderer.uniforms.rows = 1
+  // renderer.uniforms.rows = 4
 
   renderer.setMeshData([{
     positions: [
@@ -59,6 +66,12 @@ function drawGrids(canvas: HTMLCanvasElement) {
       [-1, 1],
       [1, 1],
       [1, -1],
+
+      // 只展示右上角区域
+      /* [0, 0],
+      [0, 1],
+      [1, 1],
+      [1, 0], */
     ],
     attributes: {
       uv: [
